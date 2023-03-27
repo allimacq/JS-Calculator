883374b6c6f4daa579ec06ba9f2b41fa91e8d108
Allison <allimacq@gmail.com>
2023-03-27 13:12:49 -0400

    create operation functions for calculator

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const screen = document.querySelector(".screen");
 const calcBtns = document.querySelectorAll(".math");
 const clear = document.querySelector("#clear");
 const equalSign = document.querySelector("#equal-sign");
+
+//parse.Int() bc user input is string
+const sum = (a, b) => {
+    return parseInt(a) + parseInt(b);
+}
+const subtract = (a, b) => {
+    return parseInt(a) - parseInt(b);
+}
+const multiply = (a, b) => {
+    return parseInt(a) * parseInt(b);
+}
+const divide = (a, b) => {
+    return parseInt(a) / parseInt(b);
+}
