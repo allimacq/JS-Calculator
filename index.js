4124a58922732d433550b730194fc90bbf91c704
Allison <allimacq@gmail.com>
2023-03-27 13:09:07 -0400

    get keypad buttons and screen from html

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 //initializing user input string for calculations
 let userStr = "";
+
+//Getting buttons and screen from HTML
+const screen = document.querySelector(".screen");
+const calcBtns = document.querySelectorAll(".math");
+const clear = document.querySelector("#clear");
+const equalSign = document.querySelector("#equal-sign");
