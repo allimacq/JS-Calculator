e2f71ab01b611361290fd739f34a451c8f952a9f
Allison <allimacq@gmail.com>
2023-03-27 14:09:59 -0400

    add solution()

--- index.js
+++ index.js
@@ -20,3 +20,23 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     return parseInt(a) / parseInt(b);
 }
+
+const solution = (userStr) => {
+    //turn input to array and get rid of initial empty string with filter()
+    userStr = userStr.split(" ").filter(x => x !== "")
+    //initializing a
+    let a = userStr[0];
+    for (i = 0; i < userStr.length - 2; i += 2) {
+        let b = userStr[i + 2];
+        if (userStr[i + 1] === "+") {
+            a = sum(a, b)
+        } else if (userStr[i + 1] === "-") {
+            a= subtract(a, b)
+        } else if (userStr[i + 1] === "*") {
+            a= multiply(a, b)
+        } else if (userStr[i + 1] === "/") {
+            a = divide(a, b)
+        } 
+    }
+    return a;
+}
